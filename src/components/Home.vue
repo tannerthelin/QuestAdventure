0b0103a8adc36949b3e2a055ee6fc79182686260
tannerthelin <tannerthelin@gmail.com>
2020-03-16 16:49:07 -0600

    Created homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="home-container">
+        <img src="../assets/logo.png" alt="Simple Dungeon Logo" width="182px">
+        <p>Welcome to <span class="light-text">Simple Dungeon</span> -- a text adventure game created for DGM-3780 at Utah Valley University.</p>
+        <br><br>
+        <p class="light-text">Press any key to begin.</p>
+    </div>
+</template>
+
+<style scoped>
+
+    .home-container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        margin-bottom: 60px;
+    }
+
+    p {
+        max-width: 616px;
+    }
+
+</style>
