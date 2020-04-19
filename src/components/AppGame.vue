6851beaa1c80ad7ddb4d1148984e360ec288ca6c
tannerthelin <tannerthelin@gmail.com>
2020-04-19 16:24:36 -0600

    Added skull

--- src/components/AppGame.vue
+++ src/components/AppGame.vue
@@ -1,17 +1,30 @@
 <template>
 <div class="game-wrapper">
-    <GameHeader class="game-header" />
+    <GameHeader class="game-header" v-if="hasDied === false" />
     <!-- Stretches the height of the window -->
     <div class="main-container">
         
         <!-- Constrains the main content to 700px -->
         <div class="content-container">
 
+
+            <div class="death-container" v-if="hasDied === true">
+                <img src="../assets/skull.png" alt="sword" width="65px"/>
+                <br><br>
+                <p class="death-text">You died.</p>
+                <br>
+                <p>{{currentStory.story}}</p>
+                <br>
+                <router-link to="/character">
+                <div class="button" @click="changeName(name)">Start Over</div>
+            </router-link>   
+            </div>
+
             <!-- Display the current story -->
-            <p>{{currentStory.story}}</p>
+            <p v-if="hasDied === false">{{currentStory.story}}</p>
 
             <!-- Display the current choices -->
-            <div class="choices-container">
+            <div class="choices-container" v-if="hasDied === false">
                 <div v-for="choice in currentStory.choices" :key="choice.id" class="story-choice" @click="changeStory(choice.id)">
                     <img class="triangle" src="../assets/triangle.svg" width="7px">
                     <p>{{choice.choiceText}}</p>
@@ -35,13 +48,17 @@ export default {
     data () {
         return {
             // Set the first story to the first object in the store
-            currentStory: this.$store.state.story[0] 
+            currentStory: this.$store.state.story[0],
+            hasDied: false
         }        
     },
     methods: {
         // This is how the story gets updated when the user selects a choice
         changeStory(thisID) {
             this.currentStory = this.$store.state.story[thisID];
+            if (this.currentStory.death === true) {
+                this.hasDied = true
+            }
         }
     }
 }
@@ -49,12 +66,21 @@ export default {
 
 <style scoped>
 
+.death-text {
+    color: #FF5757;
+}
+
+.death-container {
+    text-align: center;
+}
+
 .game-wrapper {
     height: 100%;
 }
 
 .main-container {
     height: 100% !important;
+   
 }
 
 .choices-container {
@@ -92,5 +118,13 @@ export default {
 .game-header {
     position: absolute;
     top: 0;
+    width: 100%;
 }
+
+.resetbutton {
+    display: flex;
+justify-content: flex-end;
+margin-bottom: 25px;
+}
+
 </style>
